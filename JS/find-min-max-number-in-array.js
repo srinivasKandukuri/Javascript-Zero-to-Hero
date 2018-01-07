8bbb4d05ff43e0994cffc3cfd28e7c70f200da6d
SK <ksrinivas.cse@gmail.com>
2018-01-07 01:56:13 -0600

    find-min-max-number

---
+++ JS/find-min-max-number-in-array.js
@@ -0,0 +1,32 @@
+var arr = [1,2,3,4,5];
+
+var sumA = [];
+for(var i=0; i<arr.length; i++){
+
+    var sum = sumVal(i, arr);
+    sumA.push(sum);
+}
+
+console.log(sumA);
+
+var max = Math.max(...sumA);
+var min = Math.min(...sumA);
+
+console.log(max + "  "+min);  
+
+
+function sumVal(i, array){
+    var newArray = array.slice();
+    newArray.splice(i, 1);
+    var sum = newArray.reduce(function(total, num){
+        return total+num;
+    })
+   return sum;    
+}
+
+/*
+O/P: 
+------------------------------
+14  10
+
+ */
