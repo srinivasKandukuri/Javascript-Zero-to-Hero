bb1ff7419992139fc4d96e7c13767394fd89be51
SK <ksrinivas.cse@gmail.com>
2018-01-07 02:00:17 -0600

    added comments

--- JS/Fibonacci Series.js
+++ JS/Fibonacci Series.js
@@ -1,10 +1,10 @@
-A Fibonacci Series of numbers will look like
+/**A Fibonacci Series of numbers will look like
 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55
 
 First two Fibonacci numbers are 0 and 1 . After that each number will be the sum of previous two numbers . Here we can see that third Fibonacci number is 1 ( sum of first two numbers 0 + 1 ) , and fourth Fibonacci number will be the sum of third and second number ( 1 + 1 = 2 ) , and fifth Fibonacci number will be the sum of fourth and third number ( 2 + 1 = 3 ). The series will go like that infinity .
+*/
 
-JS
-============
+// Solution : 1
 
 var count = 0;
 var a = 0;
@@ -22,8 +22,8 @@ while(count <5){
 
 
 
-JS
-==================
+// Solution : 2
+
 var a = 0;
 var b = 1;
 console.log(a);
