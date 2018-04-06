48770961174b601d6d194939ca2e08526d5d4a31
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2018-04-06 15:03:51 +0530

    created

---
+++ JS/FindOddNumberEvenNumber.js
@@ -0,0 +1,8 @@
+function isOdd(num) { return num % 2;}
+console.log("1 is " + isOdd(1));
+console.log("2 is " + isOdd(2));
+console.log("3 is " + isOdd(3));
+console.log("4 is " + isOdd(4));
+
+
+//1 represents an odd number, while 0 represents an even number.
