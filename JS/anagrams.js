70edbfaba0b680afb8333fe894bfc365027cd03e
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2018-04-17 18:56:08 +0530

    Create anagrams.js

---
+++ JS/anagrams.js
@@ -0,0 +1,19 @@
+
+//"Mary" is an anagram of "Army"
+
+var firstWord = "Mary";
+var secondWord = "Army";
+
+isAnagram(firstWord, secondWord); // true
+
+function isAnagram(first, second) {
+  // For case insensitivity, change both words to lowercase.
+  var a = first.toLowerCase();
+  var b = second.toLowerCase();
+
+  // Sort the strings, and join the resulting array to a string. Compare the results
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
