c412a0c5768261fbf2e07cf13326fc82af814de6
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2018-07-13 08:39:31 +0530

    Create BalancedParentheses.js

---
+++ JS/BalancedParentheses.js
@@ -0,0 +1,26 @@
+function parenthesesAreBalanced(string) {
+  var parentheses = "[]{}()",
+    stack = [],
+    i, character, bracePosition;
+
+  for(i = 0; character = string[i]; i++) {
+    bracePosition = parentheses.indexOf(character);
+
+    if(bracePosition === -1) {
+      continue;
+    }
+
+    if(bracePosition % 2 === 0) {
+      stack.push(bracePosition + 1); // push next expected brace position
+    } else {
+      if(stack.length === 0 || stack.pop() !== bracePosition) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+
+parenthesesAreBalanced("()[()]")
