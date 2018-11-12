62347ef9f7bf7ddd751e5d19a0927ea599ddd149
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2018-11-12 19:00:31 +0530

    Update Print_diagonal_elements_of_2_dimensional_array.js

--- JS/Print_diagonal_elements_of_2_dimensional_array.js
+++ JS/Print_diagonal_elements_of_2_dimensional_array.js
@@ -1,3 +1,14 @@
+
+/*. Print the diagonal elements of a 2 dimensional array using both multiple loops as well as single loop
+   Input Array: 1 2 3
+                          4 5 6
+                         7 8 9
+    Output#1: 1 5 9
+    Output#2: 3 5 7
+*/
+
+
+
 function calculateDiagonals(matrix){
     var n = matrix.length;
     var diag1 = [];
