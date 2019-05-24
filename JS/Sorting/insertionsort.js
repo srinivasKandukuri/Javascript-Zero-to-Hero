2cec4636869f51aa9abbc5fd3ff4de6e933bb5bd
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2019-05-24 11:12:13 +0530

    Create insertionsort.js

---
+++ JS/Sorting/insertionsort.js
@@ -0,0 +1,17 @@
+var numArray = [140000, 104, 99,1000,200];
+
+for (var i = 0; i < numArray.length; i++) {
+    var target = numArray[i];
+    for (var j = i - 1; j >= 0 && (numArray[j] > target); j--) {
+        numArray[j+1] = numArray[j];
+    }
+    numArray[j+1] = target
+}
+
+console.log(numArray);
+
+//[99, 104, 200, 1000, 140000]
+
+
+//Ref# https://www.geeksforgeeks.org/insertion-sort/
+
