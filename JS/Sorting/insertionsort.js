3a5486a6a426eb587b48ed04caec165563cd7138
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2019-05-24 11:14:21 +0530

    Update insertionsort.js

--- JS/Sorting/insertionsort.js
+++ JS/Sorting/insertionsort.js
@@ -1,3 +1,8 @@
+
+//Ref# https://www.geeksforgeeks.org/insertion-sort/
+
+/* . Ascending */
+
 var numArray = [140000, 104, 99,1000,200];
 
 for (var i = 0; i < numArray.length; i++) {
@@ -13,5 +18,15 @@ console.log(numArray);
 //[99, 104, 200, 1000, 140000]
 
 
-//Ref# https://www.geeksforgeeks.org/insertion-sort/
 
+/* Descending */
+
+var numArray = [140000, 104, 99];
+for (var i = 0; i < numArray.length; i++) {
+    var target = numArray[i];
+    for (var j = i - 1; j >= 0 && (numArray[j] < target); j--) {
+        numArray[j+1] = numArray[j];
+    }
+    numArray[j+1] = target
+}
+console.log(numArray);
