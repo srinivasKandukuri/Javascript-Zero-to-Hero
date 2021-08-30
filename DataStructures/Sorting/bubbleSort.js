76f7066f675737554779b797fff894fc4d44c6e2
SK <ksrinivas.cse@gmail.com>
2021-08-30 10:52:31 +0530

    bbsort

--- DataStructures/Sorting/bubbleSort.js
+++ DataStructures/Sorting/bubbleSort.js
@@ -2,25 +2,25 @@
 
 //Concept 1 Code
 
-function bsort(arr){
-    var done = false;
-    while(!done){
-        done = true;
-        for(var i=0; i< arr.length; i++){
-            if(arr[i] > arr[i+1]){
-                done = false;
-                let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1]= temp;
-            }
-        }
+function bsort(arr) {
+  var done = false;
+  while (!done) {
+    done = true;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        done = false;
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
     }
+  }
 
-   return arr;
-  
+  return arr;
+
 }
 
-console.log(bsort([3,4,2,10,66,99,1]));
+console.log(bsort([3, 4, 2, 10, 66, 99, 1]));
 
 
 // Bubble Sort is one of the most inefficient sorting algorithms coming in at O(N²). 
@@ -30,16 +30,41 @@ console.log(bsort([3,4,2,10,66,99,1]));
 //Concept 2 Code
 
 function bubbleSortConcept1(arr) {
-  for (let j=arr.length-1; j>0; j--){
+  for (let j = arr.length - 1; j > 0; j--) {
     for (let i = 0; i < j; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-      } 
+      }
     }
   }
   return arr;
 }
 
-bubbleSortConcept1([20,3,40,5,6,22,7])
+bubbleSortConcept1([20, 3, 40, 5, 6, 22, 7])
+
+
+
+
+
+
+//practice
+
+function bbsort(arr) {
+  let flag = false;
+  while (!flag) {
+    flag = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        flag = false;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bbsort([85, 7, 3, 56, 787, 4]));
