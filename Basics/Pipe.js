521e4c0b50f0f0472fd175055d922320f1c63d95
Srinivas Kandukuri <srinivasKandukuri@users.noreply.github.com>
2021-09-17 00:04:35 +0530

    compose

--- Basics/Pipe.js
+++ Basics/Pipe.js
@@ -1,3 +1,6 @@
+// Ref# https://medium.com/free-code-camp/pipe-and-compose-in-javascript-5b04004ac937
+
+
 var g = n+1;
 var f = n*2;
 
@@ -9,15 +12,16 @@ var h = (value) => f(g(value));
 var compose1 = (f,g) => x => f(g(x));
 //compose(f,g)(20);
 
+/// compose right to left
 var compose2 = (...fns) => x => fns.reduceRight((y,f) => f(y),x);
 //compose2(f,g)(20);
 
-
-// Explanation of es6
+// pipe left to right
+// Explanation of es6 
 var pipe = (...functions) => (value) => {
     debugger;
     return functions
-      .reduceRight((currentValue, currentFunction) => {
+      .reduce((currentValue, currentFunction) => {
          debugger;
          return currentFunction(currentValue);
       }, value)
